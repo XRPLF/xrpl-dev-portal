f9e9ea7da8659fa82c1bda84fac86ec20988122d
Norman Hsieh <normanhsieh@gmail.com>
2014-04-21 17:55:19 -0700

    lots of changes

---
+++ www/js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+	$('.header > .left > .header-menu-button').on("click", function(e){
+
+	    e.preventDefault();
+
+	    $('.header > .left > ul').toggleClass('active');
+	});
+
+	$(".content-root").on('click', function(){
+
+	  $('.header > .left > ul').removeClass('active');
+
+	});
+	$(".modal-checkbox input").on("change",function(){
+		$("#btn-download").toggleClass("btn-download btn-disabled");
+	});
+	$("#btn-download").click(function(){
+		if($(this).hasClass("btn-disabled")){
+			return false;
+		}
+	});
+	$(".logo-download-link").click(function(){
+		var filename = $(this).data("src");
+		$("#btn-download").attr("href", filename).attr("download", filename.replace(/^.*[\\\/]/, ''));
+	});
+
+});
+
