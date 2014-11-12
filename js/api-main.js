e514f256b3c7774d5f54d3f55c68bf115bf4384c
mDuo13 <mduo13@gmail.com>
2014-11-11 17:26:18 -0800

    [FIX] WS API tool - removed deprecated account_tx fields; removed unused min.js file

--- js/api-main.js
+++ js/api-main.js
@@ -184,8 +184,6 @@
     ledger_index_max:  -1,
     binary:            false,
     count:             false,
-    descending:        false,
-    offset:            0,
     limit:             10,
     forward:           false,
     marker:            void(0),
