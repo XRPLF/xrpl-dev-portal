560c84f4f90a2c35a2f55a24e5f110ac0c42047d
mDuo13 <mduo13@gmail.com>
2018-03-07 14:41:33 -0800

    Fork/branch-aware edit on GH links, part 1/?

--- tool/jenkins.sh
+++ tool/jenkins.sh
@@ -2,6 +2,11 @@
 
 # meant to be run from the root directory of the repo
 
+## Test: show pull request data in build results
+echo "ghprbAuthorRepoGitUrl is: " ${ghprbAuthorRepoGitUrl}
+echo "ghprbSourceBranch is: " ${ghprbSourceBranch}
+echo "ghprbPullId is: " ${ghprbPullId}
+
 set -e
 
 tool/conflictmarkers.sh
