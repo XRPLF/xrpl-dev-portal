03d936663378ce9c5be98dd07e202472677fea01
mDuo13 <mduo13@gmail.com>
2018-05-30 17:50:59 -0700

    Fix check js prepareCreate problems

--- content/_code-samples/checks/js/prepareCreate.js
+++ content/_code-samples/checks/js/prepareCreate.js
@@ -18,7 +18,7 @@ api.connect().then(() => {
     "sendMax": {
       "currency": "XRP",
       "value": "100" // RippleAPI uses decimal XRP, not integer drops
-    }
+    },
     "invoiceID": "46060241FABCF692D4D934BA2A6C4427CD4279083E38C77CBE642243E43BE291"
   }, options)
 
