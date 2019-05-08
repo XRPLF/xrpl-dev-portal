9dbb4c49d1046bd7679e9e877e90a2f18e0c2bbd
Jake Bonham <jake@docforce.io>
2019-05-08 11:00:44 -0700

    Updated header to match new style

--- assets/js/jump-to-top.js
+++ assets/js/jump-to-top.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+  // Mobile menu.  
+  $('[data-toggle="slide-collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.toggleClass('show');
+    $(".menu-overlay").toggleClass('active');
+  });
+  $(".menu-overlay").click(function(event) {
+    $(".navbar-toggler").trigger("click");
+  });
+  
+
+
+
+
+
   var TO_TOP_MIN = 50;
   var TO_TOP_SPEED = 500;
   var TO_TOP_POS = 0;
