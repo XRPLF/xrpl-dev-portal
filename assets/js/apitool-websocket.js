e0a07a691ee2632c176f41636db5604f034ed626
mDuo13 <mduo13@gmail.com>
2019-07-02 18:36:04 -0700

    WebSocket Tool: Fix slug formula (allow _) to fix 'Try It' links

--- assets/js/apitool-websocket.js
+++ assets/js/apitool-websocket.js
@@ -16,13 +16,13 @@ function slugify(str) {
   str = str.toLowerCase()
 
   // remove accents, swap ñ for n, etc
-  const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
-  const to   = "aaaaeeeeiiiioooouuuunc------"
+  const from = "àáäâèéëêìíïîòóöôùúüûñç·/,:;"
+  const to   = "aaaaeeeeiiiioooouuuunc-----"
   for (let i=0, l=from.length ; i<l ; i++) {
     str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
   }
 
-  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+  str = str.replace(/[^a-z0-9 _-]/g, '') // remove invalid chars
     .replace(/\s+/g, '-') // collapse whitespace and replace by -
     .replace(/-+/g, '-'); // collapse dashes
 
