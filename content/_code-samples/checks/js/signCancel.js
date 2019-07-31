a541d4bc971696b149ab241e4ef18e3c4884d91e
crypto-deus <53535753+crypto-deus@users.noreply.github.com>
2019-07-31 20:40:48 +0200

    Update signCancel.js
    checkID changed to be in line with  the CheckID in the Prerequisites section and previous steps in the tutorial.

--- content/_code-samples/checks/js/signCancel.js
+++ content/_code-samples/checks/js/signCancel.js
@@ -4,7 +4,7 @@ const RippleAPI = require('ripple-lib').RippleAPI
 // Can sign offline if the txJSON has all required fields
 const api = new RippleAPI()
 
-const txJSON = '{"Account":"rBXsgNkPcDN2runsvWmwxk3Lh97zdgo9za","TransactionType":"CheckCancel","CheckID":"2E0AD0740B79BE0AAE5EDD1D5FC79E3C5C221D23C6A7F771D85569B5B91195C2","Flags":2147483648,"LastLedgerSequence":8004884,"Fee":"12","Sequence":7}'
+const txJSON = '{"Account":"rBXsgNkPcDN2runsvWmwxk3Lh97zdgo9za","TransactionType":"CheckCancel","CheckID":"49647F0D748DC3FE26BDACBC57F251AADEFFF391403EC9BF87C97F67E9977FB0","Flags":2147483648,"LastLedgerSequence":8004884,"Fee":"12","Sequence":7}'
 
 // Be careful where you store your real secret.
 const secret = 's████████████████████████████'
