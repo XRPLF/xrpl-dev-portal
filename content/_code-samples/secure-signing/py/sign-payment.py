f087f0d1bc5e4eda534aee1272a8740d2f81b6c1
Ryan G. Young <ryoung@ripple.com>
2021-03-30 15:26:12 -0700

    revise per Rome feedback

--- content/_code-samples/secure-signing/py/sign-payment.py
+++ content/_code-samples/secure-signing/py/sign-payment.py
@@ -1,32 +1,20 @@
-from xrpl.core import keypairs 
-
-# Define network client
-from xrpl.clients import JsonRpcClient
-JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
-client = JsonRpcClient(JSON_RPC_URL)
-
 # Define signer address
+import os  
+my_secret = os.getenv("MY_SECRET")
 from xrpl.wallet import Wallet
-test_wallet_1 = Wallet(seed="shrPSF6vV3v3yoND9J3NeKks6M3xd")
-print(test_wallet_1.classic_address) # "raaFKKmgf6CRZttTVABeTcsqzRQ51bNR6Q"
-
-# Prepate the tranaaction
-from xrpl.ledger import get_latest_validated_ledger_sequence
-current_validated_ledger = get_latest_validated_ledger_sequence(client)
-min_ledger = current_validated_ledger + 1
-max_ledger = min_ledger + 20
+wallet = Wallet(seed="MY_SECRET")
+print(wallet.classic_address) # "raaFKKmgf6CRZttTVABeTcsqzRQ51bNR6Q"
 
 from xrpl.models.transactions import Payment
-# not working in testing
-# from xrpl.utils import xrp_to_drops
+from xrpl.utils import xrp_to_drops
 my_payment = Payment(
     account=test_wallet_1.classic_address,
-    amount="2200000",
+    amount=xrp_to_drops(22),
+    fee=xrp_to_drops(10),
     destination="rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
-    sequence=test_wallet_1.next_sequence_num,
+    sequence="16126889",
 )
 print("Payment object:", my_payment)
-print(f"Can be validated in ledger range: {min_ledger} - {max_ledger}")
 
 # Sign transaction -------------------------------------------------------------
 import xrpl.transaction 
