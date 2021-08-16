42eb1f608e46f76a522443a4a04f965991248069
Jake <Jake@docforce.io>
2021-08-16 15:56:53 -0700

    First pass copy to clipboard

---
+++ tool/filter_copy_code_to_clipboard.py
@@ -0,0 +1,40 @@
+################################################
+## Add copy to clipboard btton
+## Author: Jake Bonham
+## Copyright: Ripple Labs, 2021
+##
+## Finds codeblocks and adds copy to clipboard button 
+################################################
+
+
+# Issues/questions
+# Multistep tutoral. > Get started page. Has 5 blocks with 4 hidden? 
+# Position on multitabs 
+# Do we need on any "output" blocks? 
+
+
+def filter_soup(soup, **kwargs):
+    """
+    1. Finds all elements with class of "codehilite" 
+    2. Adds copy to clipboard button. 
+        Button looks like >
+        <button class="clipboard-btn" data-clipboard-target="#codeblock-0" id="codeblock-0button">Copy to clipboard</button>
+    3. Adds id to <code> element. 
+    """
+
+    code_blocks = soup.find_all(class_="codehilite")
+    index1 = 0
+    for code_block in code_blocks:
+        # add id to each child <code> 
+        codeBlock = code_block.find("code")
+        codeBlock_id = "codeblock-%d" % index1
+        codeBlock["id"] = codeBlock_id
+        # Add copy button
+        new_tag = soup.new_tag('button', id=codeBlock_id+'button')
+        new_tag.append("Copy to clipboard") #TODO: localize 
+        new_tag['class'] = "clipboard-btn"
+        new_tag['data-clipboard-target'] = "#"+codeBlock_id
+        code_block.insert(0, new_tag)
+        # 
+        index1 += 1
+
