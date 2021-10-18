acef5fcccc284a2ac694b5c08443a741d5e22fcc
Jackson Mills <jmills@ripple.com>
2021-10-18 09:27:45 -0700

    Migrate check-global-freeze to 2.0

--- content/_code-samples/freeze/check-global-freeze-no-freeze.js
+++ content/_code-samples/freeze/check-global-freeze-no-freeze.js
@@ -1,23 +1,32 @@
-const {RippleAPI} = require('ripple-lib');
+// Dependencies for Node.js.
+// In browsers, use <script> tags as in the example demo.html.
+if (typeof module !== "undefined") {
+  // gotta use var here because const/let are block-scoped to the if statement.
+  var xrpl = require('xrpl')
+}
 
-const api = new RippleAPI({
-  server: 'wss://s1.ripple.com' // Public rippled server
-});
-api.on('error', (errorCode, errorMessage) => {
-  console.log(errorCode + ': ' + errorMessage);
-});
+// Connect -------------------------------------------------------------------
+async function main() {
+  console.log("Connecting to Mainnet...")
+  const client = new xrpl.Client('wss://s1.ripple.com')
+  await client.connect()
 
-const my_address = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
+  client.on('error', (errorCode, errorMessage) => {
+    console.log(errorCode + ': ' + errorMessage);
+  });
 
-api.connect().then(() => {
+  const my_address = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
+
   // Look up settings object
-  return api.getSettings(my_address);
-}).then(settings => {
+  const response = await client.request({command: 'account_info', account: my_address });
+  const settings = response.result
+
   console.log('Got settings for address', my_address);
   console.log('Global Freeze enabled?',
               (settings.globalFreeze === true));
   console.log('No Freeze enabled?', (settings.noFreeze === true));
 
-}).then(() => {
-  return api.disconnect();
-}).catch(console.error);
+  await client.disconnect()
+} 
+
+main().catch(console.error)
