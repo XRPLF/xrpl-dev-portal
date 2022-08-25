72d1dcffb0d560b6996125fe9b98f6c5698e9c84
Mylo Cyrus <irahiggins740@gmail.com>
2022-08-24 19:55:24 -0700

    Add files via upload

---
+++ wis.js
@@ -0,0 +1,6 @@
+MoreWindows
+=copy relative path<Question ID="1" Shortcut="Q1" Order="" ElementType="question" QuestionType="closed" MinResponse="1" MaxResponse="0" Anonymity="1" AllowDK="1" Translated="0" >
+<LongCaption></LongCaption>
+<Modalities>undefined<Routings>
+</Routings>
+</Question>
