88350f05502131e800207c73a01fb0d91dcac619
Wo Jake <87929946+wojake@users.noreply.github.com>
2022-09-03 13:21:57 +0000

    Optimize result's output

--- content/_code-samples/submit-and-verify/py/submit-and-verify.py
+++ content/_code-samples/submit-and-verify/py/submit-and-verify.py
@@ -24,8 +24,15 @@ tx = AccountSet(
 
 # Sign the transaction locally & submit transaction and verify its validity on the ledger
 my_tx_payment_signed = safe_sign_and_submit_transaction(transaction=tx, wallet=wallet_from_seed, client=client)
+my_tx_payment_signed = my_tx_payment_signed.result
 
-if my_tx_payment_signed.is_successful():
+result = my_tx_payment_signed["engine_result"]
+
+if result == "tesSUCCESS":
     print("Transaction successful!")
+elif result == "tefMAX_LEDGER":
+    print("Transaction failed to achieve consensus")
+elif result == "unknown":
+    print("Transaction status unknown")
 else:
-    print("Transaction failed")
+    print(f"Transaction failed with code {result}")
