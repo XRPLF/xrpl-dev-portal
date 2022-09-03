9569cb3c46beb1c122f006c4633e00e21395abbd
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-09-03 23:19:56 +0530

    Updated code to derive ed25519 key pairs

--- content/_code-samples/key-derivation/js/README.md
+++ content/_code-samples/key-derivation/js/README.md
@@ -1,23 +1,17 @@
 # JS sample to convert your seed to public & private key pair
 
-1. Type `npm i` or `yarn` in your console.
-2. Goto `main.js` and add your seed value at line 4 `const seed = "snoPBrXtMeMyMHUVTgbuqAfg1SUTb";`
-3. Your public & private key will be visible in console.
-4. Don't forget to clear console.
-
-Sample:
-
-Input:
-
-```
-ssZkdwURFMBXenJPbrpE14b6noJSu
-```
+1. Type `npm i` or `yarn` in your console
+2. Run command `node main.js`
 
 Output:
 
 ```
+Code snippet to derive ed25519 key pairs
+
+Seed value: sEdVj88qEnNj3ZqsdaJtcKD19u2M5VL
+
 {
-  privateKey: '00445D0A16DD05EFAF6D5AF45E6B8A6DE4170D93C0627021A0B8E705786CBCCFF7',
-  publicKey: '02FE9932A9C4AA2AC9F0ED0F2B89302DE7C2C95F91D782DA3CF06E64E1C1216449'
+  privateKey: 'ED75C838EE937985C3EB490D574A147F737A1795E62A9E114C59B3EA94D0FC9AE7',
+  publicKey: 'ED93D0E1460B7E5B9B976ABFB0E58D128DF04E4C29D54682E0297C8D0365EC7C12'
 }
 ```
--- content/_code-samples/key-derivation/js/main.js
+++ content/_code-samples/key-derivation/js/main.js
@@ -1,10 +1,36 @@
-const { deriveKeypair } = require("ripple-keypairs/dist");
+const { generateSeed } = require("ripple-keypairs/dist");
+const addressCodec = require('ripple-address-codec');
+const elliptic = require("elliptic");
+const hashjs = require('hash.js');
 
-// Seed value
-const seed = "snoPBrXtMeMyMHUVTgbuqAfg1SUTb";
+console.log(`Code snippet to derive ed25519 key pairs\n`);
 
-// Derive Key pairs using ripple-keypairs/dist
-const key_pair = deriveKeypair(seed);
+// Generating random ed25519 seed 
+const seed = generateSeed({ algorithm: "ed25519" });
+console.log(`Seed value: ${seed}\n`);
+
+// JS Elliptic-curve cryptographic implementation, Link: https://www.npmjs.com/package/elliptic
+const Ed25519 = elliptic.eddsa('ed25519');
+
+// Function to convert bytes to hex
+const bytesToHex = (a) => {
+    return Array.from(a, (byteValue) => {
+        const hex = byteValue.toString(16).toUpperCase()
+        return hex.length > 1 ? hex : `0${hex}`
+    }).join('');
+};
+
+// hashing the message using hash.js npm library, Link: https://github.com/indutny/hash.js
+const hash = (message) => hashjs.sha512().update(message).digest().slice(0, 32);
+
+// Decode seed using ripple-address-codec
+const decoded = addressCodec.decodeSeed(seed);
+
+// Conversion to key pairs
+const prefix = 'ED'
+const rawPrivateKey = hash(decoded.bytes)
+const privateKey = prefix + bytesToHex(rawPrivateKey)
+const publicKey = prefix + bytesToHex(Ed25519.keyFromSecret(rawPrivateKey).pubBytes());
 
 // log key pairs in console
-console.log(key_pair);
+console.log({ privateKey, publicKey });
