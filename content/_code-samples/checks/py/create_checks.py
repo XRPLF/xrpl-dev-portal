47844c01d2ff7ae38028e2f18dcc586f3ad3c385
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-09-03 04:11:48 -0700

    Update and rename create_token_check.py to create_checks.py

--- content/_code-samples/checks/py/create_checks.py
+++ content/_code-samples/checks/py/create_checks.py
@@ -10,14 +10,12 @@ client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # connect to the
 """Create a token check"""
 
 """Helper methods for working with token names"""
-
 def symbol_to_hex(symbol: str = None) -> str:
     """token symbol_to_hex."""
     if len(symbol) > 3:
         bytes_string = bytes(str(symbol).encode('utf-8'))
         return bytes_string.hex().upper().ljust(40, '0')
     return symbol
-
 
 # check sender seed
 sender_seed = "sxxxxxxxxxxxxxxxxxxxx"
@@ -47,6 +45,46 @@ send_max=IssuedCurrencyAmount(
     issuer=issuer, 
     value=amount),
     expiration=expiry_date)
+
+# sign transaction
+stxn = safe_sign_and_autofill_transaction(check_txn, sender_wallet, client)
+
+# submit transaction and wait for result
+stxn_response = send_reliable_submission(stxn, client)
+
+# parse response for result
+stxn_result = stxn_response.result
+
+# print result and transaction hash
+print(stxn_result["meta"]["TransactionResult"])
+print(stxn_result["hash"])
+
+
+
+
+############ CREATE XRP CHECK ################################
+"""Create xrp check"""
+
+# check sender seed
+sender_seed = "sxxxxxxxxxxxxxxxxxxxx"
+
+# check receiver address
+receiver_addr = "rxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# amount of xrp to deliver
+amount = 10.00
+
+# check expiry date
+expiry_date = int # from xrpl.utils import datetime_to_ripple_time()
+
+# generate wallet from seed
+sender_wallet = Wallet(seed=sender_seed, sequence=0)
+
+# build check create transaction
+check_txn = CheckCreate(account=sender_wallet.classic_address,
+        destination=receiver_addr,
+        send_max=xrp_to_drops(amount),
+        expiration=expiry_date)
 
 # sign transaction
 stxn = safe_sign_and_autofill_transaction(check_txn, sender_wallet, client)
