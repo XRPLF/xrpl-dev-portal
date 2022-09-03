f3f7c462562e4b1e9a271dbdea05a42714d9fba0
Wo Jake <87929946+wojake@users.noreply.github.com>
2022-09-03 16:30:56 +0000

    Better result's output

--- content/_code-samples/require-destination-tags/py/require-destination-tags.py
+++ content/_code-samples/require-destination-tags/py/require-destination-tags.py
@@ -24,3 +24,14 @@ tx = AccountSet(
 
 # Sign the transaction locally and submit to a node
 my_tx_payment_signed = safe_sign_and_submit_transaction(tx, wallet=wallet_from_seed, client=client)
+my_tx_payment_signed = my_tx_payment_signed.result
+result = my_tx_payment_signed["engine_result"]
+
+if result == "tesSUCCESS":
+    print("AccountSet transaction successful, Destination Tag is now required for all incoming transactions")
+elif result == "tefMAX_LEDGER":
+    print("Transaction failed to achieve consensus")
+elif result == "unknown":
+    print("Transaction status unknown")
+else:
+    print(f"Transaction failed with code {result}")
