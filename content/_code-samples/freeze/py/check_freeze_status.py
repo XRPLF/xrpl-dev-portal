3e3557c7238154ee6aa2a5b2eea147b67faf09bf
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-09-07 11:28:22 -0700

    Update check_freeze_status.py

--- content/_code-samples/freeze/py/check_freeze_status.py
+++ content/_code-samples/freeze/py/check_freeze_status.py
@@ -1,17 +1,10 @@
 from xrpl.clients import JsonRpcClient
 from xrpl.models import AccountLines
-from xrpl.wallet import Wallet, generate_faucet_wallet
+from xrpl.utils import str_to_hex
+from xrpl.wallet import generate_faucet_wallet
 
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # connect to testnetwork
 
-
-# Token helper method
-def symbol_to_hex(symbol: str = None) -> str:
-    """symbol_to_hex."""
-    if len(symbol) > 3:
-        bytes_string = bytes(str(symbol).encode('utf-8'))
-        return bytes_string.hex().upper().ljust(40, '0')
-    return symbol
 
 # issuer address
 issuer_addr =generate_faucet_wallet(client=client).classic_address
@@ -36,7 +29,7 @@ if "lines" in result:
     lines = result["lines"]
     for line in lines:
         # query result with trustline params
-        if target_addr == line["account"] and symbol_to_hex(token) == line["currency"]: 
+        if target_addr == line["account"] and str_to_hex(token) == line["currency"]: 
             if 'freeze' in line:
                print(f'freeze status of trustline: {line["freeze"]}')
             else:
