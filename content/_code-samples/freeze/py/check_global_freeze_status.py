21dbed43b3bbb975d461fd3bf3c3bbeb24074a10
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-09-23 13:12:33 -0700

    Update check_global_freeze_status.py

--- content/_code-samples/freeze/py/check_global_freeze_status.py
+++ content/_code-samples/freeze/py/check_global_freeze_status.py
@@ -4,6 +4,26 @@ from xrpl.models import AccountInfo
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # connect to testnetwork
 
 
+ACCOUNT_ROOT_LEDGER_FLAGS: dict[str, int] = {
+        "lsfPasswordSpent": 0x00010000,
+        "lsfRequireDestTag": 0x00020000,
+        "lsfRequireAuth": 0x00040000,
+        "lsfRequireAuth": 0x00040000,
+        "lsfDisableMaster": 0x00100000,
+        "lsfNoFreeze": 0x00200000,
+        "lsfGlobalFreeze": 0x00400000,
+        "lsfDefaultRipple": 0x00800000,
+        "lsfDepositAuth": 0x01000000,
+    }
+
+def parse_account_root_flags(flags: int) -> list[str]:
+    flags_enabled = []
+    for flag in ACCOUNT_ROOT_LEDGER_FLAGS:
+        check_flag = ACCOUNT_ROOT_LEDGER_FLAGS[flag]
+        if check_flag & flags == check_flag:
+            flags_enabled.append(flag)
+    return flags_enabled
+
 # issuer address to query for global freeze status
 issuer_addr = "rfDJ98Z8k7ubr6atbZoCqAPdg9MetyBwcg"
 
@@ -16,8 +36,10 @@ response = client.request(acc_info)
 # parse response for result
 result = response.result
 
+# query result for global freeze status
 if "account_data" in result:
     if "Flags" in result["account_data"]:
-        if result["account_data"]["Flags"] == 4194304:
-            print("global freeze is enabled")
-        
+        if "lsfGlobalFreeze" in parse_account_root_flags(result["account_data"]["Flags"]):
+            print("Global Freeze is enabled")
+        else:
+            print("Global Freeze is disabled")
