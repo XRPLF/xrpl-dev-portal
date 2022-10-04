2aad2e8e84545adb352faaf8ee958ce4c420605e
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-10-04 01:53:22 -0700

    Update unfreeze_token.py

--- content/_code-samples/freeze/py/unfreeze_token.py
+++ content/_code-samples/freeze/py/unfreeze_token.py
@@ -2,41 +2,38 @@ from xrpl.clients import JsonRpcClient
 from xrpl.models import IssuedCurrencyAmount, TrustSet
 from xrpl.transaction import (safe_sign_and_autofill_transaction,
                               send_reliable_submission)
+from xrpl.models import TrustSetFlag
 from xrpl.utils import str_to_hex
 from xrpl.wallet import generate_faucet_wallet
 
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # connect to testnet
 
+token_name = "USD"
 
-# token name
-token = "LegitXRP"
-
-# amount a trustline can handle
+# Amount a trustline can handle
 value = "0"
 
-# address to ufreeze trustline
+# Address to ufreeze trustline
 target_addr = generate_faucet_wallet(client=client).classic_address
 
-# generate wallet
+# Sender wallet
 sender_wallet = generate_faucet_wallet(client=client)
 
-# build trustline freeze transaction
+# Build trustline freeze transaction
 trustset = TrustSet(account=sender_wallet.classic_address, limit_amount=IssuedCurrencyAmount(
-    currency=str_to_hex(token),
+    currency=token,
     issuer=target_addr,
     value = value 
 ),
-flags=2097152)# flag to unfreeze the trust line.
+flags=TrustSetFlag.TF_CLEAR_FREEZE)# flag to unfreeze the trust line.
 
-# sign transaction
+# Sign and Submit transaction
 stxn = safe_sign_and_autofill_transaction(trustset, sender_wallet, client)
-
-# submit transaction and wait for result
 stxn_response = send_reliable_submission(stxn, client)
 
-# parse response for result
+# Parse response for result
 stxn_result = stxn_response.result
 
-# print result and transaction hash
+# Print result and transaction hash
 print(stxn_result["meta"]["TransactionResult"])
 print(stxn_result["hash"])
