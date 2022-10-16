1af48bacbd0e456771531736b3377103415a4723
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-10-15 20:40:55 -0700

    Update return_escrow_sequence.py

--- content/_code-samples/escrow/py/return_escrow_sequence.py
+++ content/_code-samples/escrow/py/return_escrow_sequence.py
@@ -1,18 +1,21 @@
 from xrpl.clients import JsonRpcClient
 from xrpl.models import Tx
 
-client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # connect to the testnetwork
+client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # Connect to the testnetwork
 
-prev_txn_id = str
+prev_txn_id = ""
 # Return escrow seq from `PreviousTxnID` for finishing or cancelling escrows
 
-req = Tx(transaction=prev_txn_id) # build query for PreviousTxnID
-response = client.request(req) # send query to the network
-result = response.result # return the result
+# Build and send query for PreviousTxnID 
+req = Tx(transaction=prev_txn_id) 
+response = client.request(req)
 
-# print escrow sequence if available
+# Return the result
+result = response.result
+
+# Print escrow sequence if available
 if "Sequence" in result:
     print(f'escrow sequence: {result["Sequence"]}')
-# use escrow ticket sequence if escrow sequence is not available
+# Use escrow ticket sequence if escrow sequence is not available
 if "TicketSequence" in result:
     print(f'escrow ticket sequence: {result["TicketSequence"]}')
