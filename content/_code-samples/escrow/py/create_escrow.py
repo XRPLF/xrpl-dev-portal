58cd14ceeb511fedefe4c4006252417948405f20
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-10-17 21:40:48 -0700

    Update create_escrow.py

--- content/_code-samples/escrow/py/create_escrow.py
+++ content/_code-samples/escrow/py/create_escrow.py
@@ -1,11 +1,11 @@
-from xrpl.wallet import Wallet, generate_faucet_wallet
-from xrpl.clients import JsonRpcClient
 from datetime import datetime, timedelta
-from xrpl.utils import xrp_to_drops
+
+from xrpl.clients import JsonRpcClient
+from xrpl.models import EscrowCreate
 from xrpl.transaction import (safe_sign_and_autofill_transaction,
                               send_reliable_submission)
-from xrpl.utils import datetime_to_ripple_time
-from xrpl.models import EscrowCreate                             
+from xrpl.utils import datetime_to_ripple_time, xrp_to_drops
+from xrpl.wallet import generate_faucet_wallet
 
 # Create Escrow
 
@@ -24,7 +24,6 @@ expiry_date = datetime_to_ripple_time(datetime.now() + timedelta(days=5))
 # Optional field
 # You can optionally use a Crypto Condition to allow for dynamic release of funds. For example:
 condition = "A02580205A0E9E4018BE1A6E0F51D39B483122EFDF1DDEF3A4BE83BE71522F9E8CDAB179810120" # do not use in production
-
 
 # sender wallet object
 sender_wallet = generate_faucet_wallet(client=client)
