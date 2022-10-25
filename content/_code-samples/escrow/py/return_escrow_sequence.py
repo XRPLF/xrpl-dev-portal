8b7babf2abe415de8c073030654147c125456b05
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-10-25 02:23:37 -0700

    Update return_escrow_sequence.py

--- content/_code-samples/escrow/py/return_escrow_sequence.py
+++ content/_code-samples/escrow/py/return_escrow_sequence.py
@@ -3,8 +3,11 @@ from xrpl.models import Tx
 
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234") # Connect to the testnetwork
 
-prev_txn_id = ""
+
+prev_txn_id = "" # should look like this '84503EA84ADC4A65530C6CC91C904FCEE64CFE2BB973C023476184288698991F'
 # Return escrow seq from `PreviousTxnID` for finishing or cancelling escrows
+if prev_txn_id == "":
+    print("No transaction id provided. Use create_escrow.py to generate an escrow transaction, then you can look it up by modifying prev_txn_id to use that transaction's id.")
 
 # Build and send query for PreviousTxnID 
 req = Tx(transaction=prev_txn_id) 
