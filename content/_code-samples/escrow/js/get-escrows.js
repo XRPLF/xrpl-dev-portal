d4b1247028792cdba79cc5cbb41617c43c33de16
Tushar Pardhe <tpardhe@gmail.com>
2023-02-02 22:49:41 +0000

    Added logs & comments

--- content/_code-samples/escrow/js/get-escrows.js
+++ content/_code-samples/escrow/js/get-escrows.js
@@ -1,57 +1,75 @@
 'use strict'
 if (typeof module !== "undefined") {
-  // Use var here because const/let are block-scoped to the if statement.
-  var xrpl = require('xrpl');
+    // Use var here because const/let are block-scoped to the if statement.
+    var xrpl = require('xrpl');
 };
 
 const address = "rNQhxJsQ5a4newq4acmWsrn8PbrSjLu69p";
-const source = ""; // Optional: filter by source
-const destination = ""; // Optional: filter by destination
+const sourceAddress = ""; // Optional: filter by source address
+const destinationAddress = ""; // Optional: filter by destination address
 
 const main = async () => {
-    const client = new xrpl.Client('wss://s.altnet.rippletest.net:51233');
-    await client.connect();
-    let currMarker = null;
-    let account_objects = [];
-    
-    // Loop through all account objects until marker is undefined
-    do {
 
-        const payload = {
-            "command": "account_objects",
-            "account": address,
-            "ledger_index": "validated",
-            "type": "escrow",
+    try {
+
+        // Connect to Testnet -----------------------------------------------
+        console.log("Connecting to testnet...");
+        const client = new xrpl.Client('wss://s.altnet.rippletest.net:51233');
+        await client.connect();
+        console.log("Connected.");
+        
+        let currMarker = null;
+        let account_objects = [];
+        console.log("Getting escrows...");
+        // Loop through all account objects until marker is undefined -------
+        do {
+
+            const payload = {
+                "command": "account_objects",
+                "account": address,
+                "ledger_index": "validated",
+                "type": "escrow",
+            };
+
+            if (currMarker !== null) {
+                payload.marker = currMarker;
+            };
+
+            let { result: { account_objects: escrows, marker } } = await client.request(payload);
+
+            if (marker === currMarker) {
+                break;
+            }
+
+            account_objects.push(...escrows);
+            currMarker = marker;
+        } while (typeof currMarker !== 'undefined')
+
+        // Filter by source address ----------------------------------------
+        if (sourceAddress.length > 0) {
+            account_objects = account_objects.filter((escrow) => escrow.Account === sourceAddress);
         };
 
-        if(currMarker !== null) {
-            payload.marker = currMarker;
+        // Filter by destination address -----------------------------------
+        if (destinationAddress.length > 0) {
+            account_objects = account_objects.filter((escrow) => escrow.Destination === destinationAddress);
         };
 
-        let { result: { account_objects : escrows, marker } } = await client.request(payload);
+        // Print results ---------------------------------------------------
+        if (account_objects.length === 0) {
+            console.log("No escrows found.");
+            return;
+        };
 
-        if(marker === currMarker){
-            break;
+        if(sourceAddress.length === 0 && destinationAddress.length === 0) {
+            console.log("Note: The sourceAddress and destinationAddress variables can be used to filter escrows by source or destination address.");
         }
 
-        account_objects.push(...escrows);
-        currMarker =  marker;
-    } while (typeof currMarker !== 'undefined')
+        console.log(account_objects);
+    } catch (err) {
+        console.log(err);
+    }
 
-    if (source.length > 0) {
-        account_objects = account_objects.filter((escrow) =>  escrow.Account === source);
-    };
-
-    if (destination.length > 0) {
-        account_objects = account_objects.filter((escrow) =>  escrow.Destination === destination);
-    };
-
-    if(account_objects.length === 0) {
-        console.log("No escrows found.");
-        return;
-    };
-
-    console.log(account_objects);
 };
 
 main();
