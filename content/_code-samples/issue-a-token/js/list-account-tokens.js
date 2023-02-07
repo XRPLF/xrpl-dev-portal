5dd90c9fd1c3bea979f4963d5c352b47df59d5d5
Wo Jake <87929946+wojake@users.noreply.github.com>
2023-02-07 23:54:40 +0000

    Update list-account-tokens.js

--- content/_code-samples/issue-a-token/js/list-account-tokens.js
+++ content/_code-samples/issue-a-token/js/list-account-tokens.js
@@ -22,7 +22,7 @@ if (typeof module !== "undefined") {
         ledger_index: "validated",
     })
 
-    const response_lines = await client.request({
+    const responseLines = await client.request({
         command: "account_lines",
         account: account,
         ledger_index: "validated",
@@ -30,27 +30,27 @@ if (typeof module !== "undefined") {
 
     // We'll only display the key data of RippleState ledger objects, other objects are simply shown as their ObjectID and their LedgerEntryType
     console.log(`\nAccount ${account}'s Ledger Objects:`)
-    for (let i = 0; i < response_objects.result.account_objects.length; i++) {
-        if (response_objects.result.account_objects[i].LedgerEntryType != "RippleState") {
-            console.log(`\n${i+1}. Index (ObjectID/keylet): ${response_objects.result.account_objects[i].index}`)
-            console.log(` - LedgerEntryType: ${response_objects.result.account_objects[i].LedgerEntryType}`)
-            console.log(` - Account: ${response_objects.result.account_objects[i].Account})`)
-            console.log(` - Destination: ${response_objects.result.account_objects[i].Destination}`)
-            console.log(` - Amount: ${response_objects.result.account_objects[i].Amount} drops`)
+    for (let i = 0; i < responseObjects.result.account_objects.length; i++) {
+        if (responseObjects.result.account_objects[i].LedgerEntryType != "RippleState") {
+            console.log(`\n${i+1}. Index (ObjectID/keylet): ${responseObjects.result.account_objects[i].index}`)
+            console.log(` - LedgerEntryType: ${responseObjects.result.account_objects[i].LedgerEntryType}`)
+            console.log(` - Account: ${responseObjects.result.account_objects[i].Account})`)
+            console.log(` - Destination: ${responseObjects.result.account_objects[i].Destination}`)
+            console.log(` - Amount: ${responseObjects.result.account_objects[i].Amount} drops`)
         } else {
-            console.log(`\n${i+1}. Index (ObjectID/keylet): ${response_objects.result.account_objects[i].index}`)
-            console.log(` - LedgerEntryType: ${response_objects.result.account_objects[i].LedgerEntryType} `)
+            console.log(`\n${i+1}. Index (ObjectID/keylet): ${responseObjects.result.account_objects[i].index}`)
+            console.log(` - LedgerEntryType: ${responseObjects.result.account_objects[i].LedgerEntryType} `)
         }
     }
 
     console.log(`\nAccount ${account}'s Trust lines:`)
-    for (let i = 0 ; i < response_lines.result.lines.length; i++) {
+    for (let i = 0 ; i < responseLines.result.lines.length; i++) {
         console.log(`\n${i+1}. Trustline:`)
-        console.log(` - Account: ${response_lines.result.lines[i].account}`)
-        console.log(` - Currency: ${response_lines.result.lines[i].currency}`)
-        console.log(` - Amount: ${response_lines.result.lines[i].balance}`)
-        console.log(` - Limit: ${response_lines.result.lines[i].limit}`)
-        console.log(` - Limit Peer: ${response_lines.result.lines[i].limit_peer}`)
+        console.log(` - Account: ${responseLines.result.lines[i].account}`)
+        console.log(` - Currency: ${responseLines.result.lines[i].currency}`)
+        console.log(` - Amount: ${responseLines.result.lines[i].balance}`)
+        console.log(` - Limit: ${responseLines.result.lines[i].limit}`)
+        console.log(` - Limit Peer: ${responseLines.result.lines[i].limit_peer}`)
     }
     client.disconnect()
     // End main()
