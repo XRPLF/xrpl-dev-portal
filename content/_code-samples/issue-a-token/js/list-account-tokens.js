74c6708086f5d5d9c58ba6a313056fd1a327df35
Wo Jake <87929946+wojake@users.noreply.github.com>
2023-02-08 02:18:33 +0000

    fix indentation

--- content/_code-samples/issue-a-token/js/list-account-tokens.js
+++ content/_code-samples/issue-a-token/js/list-account-tokens.js
@@ -1,33 +1,33 @@
 if (typeof module !== "undefined") {
     // Use var here because const/let are block-scoped to the if statement.
     var xrpl = require('xrpl')
-  }
-  // Show tokens and other objects owned by an account.
-  // https://xrpl.org/account_objects.html#account_objects
-  // https://xrpl.org/account_lines.html#account_lines
-
-  async function main() {
+}
+// Show tokens and other objects owned by an account.
+// https://xrpl.org/account_objects.html#account_objects
+// https://xrpl.org/account_lines.html#account_lines
+ 
+async function main() {
     // Testnet example: r9CEVt4Cmcjt68ME6GKyhf2DyEGo2rG8AW
     account = "r9CEVt4Cmcjt68ME6GKyhf2DyEGo2rG8AW"
-
+ 
     // Connect to a testnet node
     console.log("Connecting to Testnet...")
     const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233/")
     await client.connect()
-
+ 
     // Query the ledger for an account's objects and its trustlines...
     const responseObjects = await client.request({
         command: "account_objects",
         account: account,
         ledger_index: "validated",
     })
-
+ 
     const responseLines = await client.request({
         command: "account_lines",
         account: account,
         ledger_index: "validated",
     })
-
+ 
     // We'll only display the key data of RippleState ledger objects, other objects are simply shown as their ObjectID and their LedgerEntryType
     console.log(`\nAccount ${account}'s Ledger Objects:`)
     for (let i = 0; i < responseObjects.result.account_objects.length; i++) {
@@ -42,7 +42,7 @@ if (typeof module !== "undefined") {
             console.log(` - LedgerEntryType: ${responseObjects.result.account_objects[i].LedgerEntryType} `)
         }
     }
-
+ 
     console.log(`\nAccount ${account}'s Trust lines:`)
     for (let i = 0 ; i < responseLines.result.lines.length; i++) {
         console.log(`\n${i+1}. Trustline:`)
@@ -54,6 +54,6 @@ if (typeof module !== "undefined") {
     }
     client.disconnect()
     // End main()
-  }
-  
-  main()
+}
+ 
+main()
