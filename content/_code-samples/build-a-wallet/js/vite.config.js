2bc578bed5c6fd1896c9472cdd4e2a7dcd4b7bd1
Tushar Pardhe <31487192+TusharPardhe@users.noreply.github.com>
2023-04-28 17:15:42 +0100

    Update content/_code-samples/build-a-wallet/js/vite.config.js
    Co-authored-by: Jackson Mills <aim4math@gmail.com>

--- content/_code-samples/build-a-wallet/js/vite.config.js
+++ content/_code-samples/build-a-wallet/js/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import polyfillNode from 'rollup-plugin-polyfill-node';
 
-const viteCongig = ({ mode }) => {
+const viteConfig = ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, '', '') };
     return defineConfig({
         define: {
