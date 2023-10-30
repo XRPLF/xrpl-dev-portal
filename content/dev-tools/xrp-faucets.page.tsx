7f9542ecc25470ebc4a87b1fb6eec300e95b06ca
JST5000 <aim4math@gmail.com>
2023-10-30 15:49:20 -0700

    Use React for modal form

--- content/dev-tools/xrp-faucets.page.tsx
+++ content/dev-tools/xrp-faucets.page.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { useTranslate } from '@portal/hooks';
+import { useState } from 'react';
 
-const target= {prefix: ''}; // TODO: fixme
+function onFaucetRadioClick() {
+
+}
 
 export default function XRPFaucets() {
   const currentpage = { prefix: "" } // TODO: Figure out what currentpage should actually be (seems to be Dactyl related) - Jackson
@@ -33,36 +36,38 @@ export default function XRPFaucets() {
     }
   ]
 
+  const [selectedFaucet, setSelectedFaucet] = useState(faucets[0])
+
   const { translate } = useTranslate();
 
   return (
     <div className="">
       <section className="container-fluid pt-3 p-md-3">
-    <h1>XRP Faucets</h1>
-    <div className="content">
-        <p>These <a href="parallel-networks.html">parallel XRP Ledger test networks</a> provide platforms for testing changes to the XRP Ledger and software built on it, without using real funds.</p>
-        <p>These funds are intended for <strong>testing</strong> only. Test networks' ledger history and balances are reset as necessary. Devnets may be reset without warning.</p>
-        <p>All balances and XRP on these networks are separate from Mainnet. As a precaution, do not use the Testnet or Devnet credentials on the Mainnet.</p>
-        <h3>Choose Network:</h3>
-         { faucets.map(net => (
-        <div className="form-check">
-            {/* TODO: Figure out why this line is borked - Jackson -> <input 1 onclick="document.getElementById('generate-creds-button').innerHTML='Generate {net.shortname} credentials'" className="form-check-input" type="radio" name="faucet-selector" id={net.id} defaultValue={net.faucet} data-jsonrpcurl={net.jsonrpc_url} data-wsurl={net.ws_url} data-shortname={net.shortname} $$ if loop.index="=" checked$$ endif /> */}
-            <label className="form-check-label" htmlFor={net.id}><strong>{net.shortname}</strong>: {net.desc}</label>
+        <h1>XRP Faucets</h1>
+        <div className="content">
+            <p>These <a href="parallel-networks.html">parallel XRP Ledger test networks</a> provide platforms for testing changes to the XRP Ledger and software built on it, without using real funds.</p>
+            <p>These funds are intended for <strong>testing</strong> only. Test networks' ledger history and balances are reset as necessary. Devnets may be reset without warning.</p>
+            <p>All balances and XRP on these networks are separate from Mainnet. As a precaution, do not use the Testnet or Devnet credentials on the Mainnet.</p>
+            <h3>Choose Network:</h3>
+            { faucets.map((net, index) => (
+            <div className="form-check">
+                <input onClick={() => setSelectedFaucet(net)} className="form-check-input" type="radio" name="faucet-selector" id={net.id} defaultValue={net.faucet} data-jsonrpcurl={net.jsonrpc_url} data-wsurl={net.ws_url} data-shortname={net.shortname} checked={selectedFaucet.shortname == net.shortname} />
+                <label className="form-check-label" htmlFor={net.id}><strong>{net.shortname}</strong>: {net.desc}</label>
+            </div>
+            )) }
+            <p className="mb-3"><strong>Hooks Testnet</strong>: <a href="https://hooks-testnet-v3.xrpl-labs.com/" className="external-link">See the Hooks Faucet</a></p>
+            <div className="btn-toolbar" role="toolbar" aria-label="Button">
+                <button id="generate-creds-button" className="btn btn-primary mr-2 mb-2">Generate {selectedFaucet.shortname} credentials</button>
+            </div>
+            {/* Displays after account is funded */}
+            <div id="your-credentials" />
+            <div id="loader" style={{display: 'none'}}><img alt="(loading)" className="throbber" src={currentpage.prefix + "static/img/xrp-loader-96.png"} /> Generating Keys...</div>
+            <div id="address" />
+            <div id="secret" />
+            <div id="balance" />
+            <div id="sequence"> <div id="loader" style={{display: 'none'}}><img alt="(loading)" className="throbber" src={currentpage.prefix + "static/img/xrp-loader-96.png"} /> Waiting...</div></div>
         </div>
-        )) }
-        <p className="mb-3"><strong>Hooks Testnet</strong>: <a href="https://hooks-testnet-v3.xrpl-labs.com/" className="external-link">See the Hooks Faucet</a></p>
-        <div className="btn-toolbar" role="toolbar" aria-label="Button">
-            <button id="generate-creds-button" className="btn btn-primary mr-2 mb-2">Generate Testnet credentials</button>
-        </div>{/*/.btn-toolbar*/}
-        <div id="your-credentials" />
-        <div id="loader" style={{display: 'none'}}><img alt="(loading)" className="throbber" src={currentpage.prefix + "static/img/xrp-loader-96.png"} /> Generating Keys...</div>
-        <div id="address" />
-        <div id="secret" />
-        <div id="balance" />
-        <div id="sequence"> <div id="loader" style={{display: 'none'}}><img alt="(loading)" className="throbber" src={currentpage.prefix + "static/img/xrp-loader-96.png"} /> Waiting...</div></div>
-    </div>
-</section>
-
+      </section>
     </div>
   )
 }
