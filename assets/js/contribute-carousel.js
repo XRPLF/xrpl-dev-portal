ca53c6e7d1d1d6855fcd4ede91f6c7633cbfc84b
akcodez <ariakesh@yahoo.com>
2023-11-06 09:33:42 -0800

    adds silky smooth animation to carousel

--- assets/js/contribute-carousel.js
+++ assets/js/contribute-carousel.js
@@ -54,27 +54,55 @@ const events = [
 
 let currentIndex = 1;
 
+let isFirstLoad = true; // Add a flag to determine if it's the initial load
+
 function updateCarousel() {
-    if(!!events[currentIndex - 1]) {
-        document.getElementById('prev-btn').style = 'opacity:1;'
-        document.getElementById('left-image').style = 'visibility:visible;'
-        document.getElementById('left-image').src = `assets/img/events/${events[currentIndex - 1].image}`;
-    } else {
-        document.getElementById('prev-btn').style = 'opacity:0.5;'
-        document.getElementById('left-image').style = 'visibility:hidden;'
+    const centerImage = document.getElementById('center-image');
+    const leftImage = document.getElementById('left-image');
+    const rightImage = document.getElementById('right-image');
+
+    if (!isFirstLoad) {
+        // Only run the exit animation if it's not the first load
+        centerImage.classList.add('exit');
+        leftImage.classList.add('exit');
+        rightImage.classList.add('exit');
     }
-    document.getElementById('center-image').src = `assets/img/events/${events[currentIndex].image}`;
-    if(!!events[currentIndex + 1] ) {
-        document.getElementById('next-btn').style = 'opacity:1;'
-        document.getElementById('right-image').style = 'visibility:visible;'
-        document.getElementById('right-image').src = `assets/img/events/${events[currentIndex + 1].image}`;
-    } else {
-        document.getElementById('next-btn').style = 'opacity:0.5;'
-        document.getElementById('right-image').style = 'visibility:hidden;'
-    }
-    document.getElementById('event-name').textContent = events[currentIndex].name;
-    document.getElementById('event-location').textContent = events[currentIndex].location;
-    document.getElementById('event-date').textContent = events[currentIndex].date;
+
+    setTimeout(() => {
+        if (events[currentIndex - 1]) {
+            document.getElementById('prev-btn').style.opacity = 1;
+            leftImage.style.visibility = 'visible';
+            leftImage.src = `assets/img/events/${events[currentIndex - 1].image}`;
+        } else {
+            document.getElementById('prev-btn').style.opacity = 0.5;
+            leftImage.style.visibility = 'hidden';
+        }
+
+        centerImage.src = `assets/img/events/${events[currentIndex].image}`;
+        
+        if (events[currentIndex + 1]) {
+            document.getElementById('next-btn').style.opacity = 1;
+            rightImage.style.visibility = 'visible';
+            rightImage.src = `assets/img/events/${events[currentIndex + 1].image}`;
+        } else {
+            document.getElementById('next-btn').style.opacity = 0.5;
+            rightImage.style.visibility = 'hidden';
+        }
+
+        document.getElementById('event-name').textContent = events[currentIndex].name;
+        document.getElementById('event-location').textContent = events[currentIndex].location;
+        document.getElementById('event-date').textContent = events[currentIndex].date;
+
+        if (!isFirstLoad) {
+            // Only run the enter animation if it's not the first load
+            centerImage.classList.remove('exit');
+            leftImage.classList.remove('exit');
+            rightImage.classList.remove('exit');
+        }
+    }, isFirstLoad ? 0 : 700); // If it's the first load, update immediately; otherwise, wait for the transition
+
+    // After the initial setup, set isFirstLoad to false
+    isFirstLoad = false;
 }
 
 document.getElementById('prev-btn').addEventListener('click', function() {
