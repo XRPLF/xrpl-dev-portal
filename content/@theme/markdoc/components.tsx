69bce3578b138c90c72ba51ad63d4fe0328009df
mDuo13 <mduo13@gmail.com>
2023-11-16 14:47:38 -0800

    Complete repo-link component

--- content/@theme/markdoc/components.tsx
+++ content/@theme/markdoc/components.tsx
@@ -3,8 +3,15 @@ import * as React from 'react';
 import dynamicReact from '@markdoc/markdoc/dist/react';
 import { Link } from '@portal/Link';
 
-export function RepoLink(props: { children: React.ReactNode; path: string }) {
-    const href = "https://github.com/XRPLF/xrpl-dev-portal/tree/master/"+path
+export function RepoLink(props: {
+        children: React.ReactNode;
+        path: string;
+        github_fork: string;
+        github_branch: string
+    }) {
+    const treeblob = props.path.indexOf(".") >= 0 ? "blob/" : "tree/"
+    const sep = props.github_fork[-1] == "/" ? "" : "/"
+    const href = props.github_fork+sep+treeblob+props.github_branch+"/"+props.path
 
     return (
         <Link to={href}>{dynamicReact(props.children, React, {})}</Link>
--- content/@theme/markdoc/schema.ts
+++ content/@theme/markdoc/schema.ts
@@ -7,6 +7,21 @@ export const repoLink: Schema & { tagName: string } = {
         type: 'String',
         required: true,
       },
+      github_fork: {
+        type: 'String',
+        required: false,
+      },
+      github_branch: {
+        type: 'String',
+        required: false,
+      },
+    },
+    transform(node, config) {
+        const attributes = node.transformAttributes(config);
+        attributes["github_fork"] = attributes["github_fork"] || config.variables.env.PUBLIC_GITHUB_FORK;
+        attributes["github_branch"] = attributes["github_branch"] || config.variables.env.PUBLIC_GITHUB_BRANCH;
+        const children = node.transformChildren(config);
+        return new Tag(this.render, attributes, children);
     },
     render: 'RepoLink',
   };
