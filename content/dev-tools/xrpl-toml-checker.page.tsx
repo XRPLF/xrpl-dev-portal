ad63c0bb9dd1ad9395321ed3c549c4574dedf5bd
JST5000 <aim4math@gmail.com>
2023-11-28 11:58:11 -0800

    WIP: Add types and uncomment new code

--- content/dev-tools/xrpl-toml-checker.page.tsx
+++ content/dev-tools/xrpl-toml-checker.page.tsx
@@ -14,35 +14,50 @@ const TIPS_2 = <p>Make sure the account has the Domain field set.</p>
 const CLASS_GOOD = "badge badge-success"
 const CLASS_BAD = "badge badge-danger"
 
-const ACCOUNT_FIELDS = [
-  "address",
-  "network",
-  "desc"
-]
-const VALIDATOR_FIELDS = [
-  "public_key",
-  "network",
-  "owner_country",
-  "server_country",
-  "unl"
-]
-const PRINCIPAL_FIELDS = [
-  "name",
-  "email"
-]
-const SERVER_FIELDS = [
-  "json_rpc",
-  "ws",
-  "peer",
-  "network"
-]
-const CURRENCY_FIELDS = [
-  "code",
-  "display_decimals",
-  "issuer",
-  "network",
-  "symbol"
-]
+interface AccountFields {
+  address: string,
+  network: string,
+  desc: string
+}
+
+interface ValidatorFields {
+  public_key: string,
+  network: string,
+  owner_country: string,
+  server_country: string,
+  unl: string
+}
+
+interface PrincipalFields {
+  name: string,
+  email: string,
+}
+
+interface ServerFields {
+  json_rpc: string,
+  ws: string,
+  peer: string,
+  network: string,
+}
+
+interface CurrencyFields {
+  code: string,
+  display_decimals: string,
+  issuer: string,
+  network: string,
+  symbol: string
+}
+
+interface XrplToml {
+  ACCOUNTS?: AccountFields[],
+  VALIDATORS?: ValidatorFields[],
+  PRINCIPALS?: PrincipalFields[],
+  SERVERS?: ServerFields[],
+  CURRENCIES?: CurrencyFields[],
+  METADATA?: {
+    modified: Date
+  }
+}
 
 function VerificationError(message, tips) {
   this.name = "VerificationError"
@@ -100,7 +115,7 @@ async function parseXRPLTomlWallet(
       id: parsingTomlId,
   }
   addNewLogEntry(setLogEntries, parsingTomlLogEntry)
-  let parsed
+  let parsed: XrplToml
   try {
     parsed = TOML.parse(tomlData)
     updateLogEntry(setLogEntries, {...parsingTomlLogEntry, response: {
@@ -263,104 +278,117 @@ async function parseXRPLTomlWallet(
 }
 
 // TODO: Migrate this one next :)
-// async function parseXRPLToml(
-//     setLogEntries: React.Dispatch<React.SetStateAction<JSX.Element[]>>, 
-//     data, 
-//     domain: string) {
-//     let parsed
-//     let logTOML = makeLogEntry("Parsing TOML data...")
-//     try {
-//       parsed = TOML(data)
-//       logTOML.resolve("SUCCESS").addClass(CLASS_GOOD)
-//     } catch(e) {
-//       logTOML.resolve(e).addClass(CLASS_BAD)
-//       return
-//     }
+// A lot of this is actually the same as above
+async function parseXRPLToml(
+    setLogEntries: React.Dispatch<React.SetStateAction<JSX.Element[]>>, 
+    data, 
+    domain: string) {
+    let parsed: XrplToml
+    let logTOML = makeLogEntry("Parsing TOML data...")
+    try {
+      parsed = TOML.parse(data)
+      logTOML.resolve("SUCCESS").addClass(CLASS_GOOD)
+    } catch(e) {
+      logTOML.resolve(e).addClass(CLASS_BAD)
+      return
+    }
   
-//     if (parsed.hasOwnProperty("METADATA")) {
-//       const metadata_type = makeLogEntry("Metadata section: ")
-//       if (Array.isArray(parsed.METADATA)) {
-//         metadata_type.resolve("Wrong type - should be table").addClass(CLASS_BAD)
-//       } else {
-//         metadata_type.resolve("Found").addClass(CLASS_GOOD)
+    if (parsed.hasOwnProperty("METADATA")) {
+      const metadata_type = makeLogEntry("Metadata section: ")
+      if (Array.isArray(parsed.METADATA)) {
+        metadata_type.resolve("Wrong type - should be table").addClass(CLASS_BAD)
+      } else {
+        metadata_type.resolve("Found").addClass(CLASS_GOOD)
   
-//         if (parsed.METADATA.modified) {
-//           const mod_log = makeLogEntry("Modified date: ")
-//           try {
-//             mod_log.resolve(parsed.METADATA.modified.toISOString()).addClass(CLASS_GOOD)
-//           } catch(e) {
-//             mod_log.resolve("INVALID").addClass(CLASS_BAD)
-//           }
-//         }
-//       }
-//     }
+        if (parsed.METADATA.modified) {
+          const mod_log = makeLogEntry("Modified date: ")
+          try {
+            mod_log.resolve(parsed.METADATA.modified.toISOString()).addClass(CLASS_GOOD)
+          } catch(e) {
+            mod_log.resolve("INVALID").addClass(CLASS_BAD)
+          }
+        }
+      }
+    }
   
-//     async function listEntries(name, list, fields, validate) {
-//       let list_wrap = $("<p>"+name+"</p>")
-//       let list_ol = $("<ol>").appendTo(list_wrap)
-//       for (i=0; i<list.length; i++) {
-//         let entry_wrap = $("<li>").appendTo(list_ol)
-//         let entry_def = $("<ul class='mb-3'>").appendTo(entry_wrap)
-//         let entry = list[i]
-//         for (j=0; j<fields.length; j++) {
-//           let fieldname = fields[j]
-//           if (entry[fieldname] !== undefined) {
-//             let field_def = $("<li><strong>"+fieldname+": </strong>").appendTo(entry_def)
-//             $(" <span class='"+fieldname+"'>").text(entry[fieldname]).appendTo(field_def)
-//           }
-//         }
-//         if (validate) {
-//           validate(entry).then((validated) => {
-//             if (validated === true) {
-//               entry_def.append('<li class="badge badge-success">Domain Validated <i class="fa fa-check-circle"></i></li>')
-//             }
-//           })
-//         }
-//       }
-//       makeLogEntry(list_wrap, true)
-//     }
+    // This version is very similar to the previous listEntries function, but it also has a validate function!
+    async function listEntries(name, list, fields, validate) {
+      let list_wrap = $("<p>"+name+"</p>")
+      let list_ol = $("<ol>").appendTo(list_wrap)
+      for (i=0; i<list.length; i++) {
+        let entry_wrap = $("<li>").appendTo(list_ol)
+        // NEW! - This line is different (class = 'mb-3')
+        let entry_def = $("<ul class='mb-3'>").appendTo(entry_wrap)
+        // END OF NEW
+        let entry = list[i]
+        for (j=0; j<fields.length; j++) {
+          let fieldname = fields[j]
+          if (entry[fieldname] !== undefined) {
+            let field_def = $("<li><strong>"+fieldname+": </strong>").appendTo(entry_def)
+            $(" <span class='"+fieldname+"'>").text(entry[fieldname]).appendTo(field_def)
+          }
+        }
+        // NEW! - Only used one time to say "if the account exists, all good!"
+        if (validate) {
+          validate(entry).then((validated) => {
+            if (validated === true) {
+              entry_def.append('<li class="badge badge-success">Domain Validated <i class="fa fa-check-circle"></i></li>')
+            }
+          })
+        }
+        // END OF NEW
+      }
+      makeLogEntry(list_wrap, true)
+    }
   
-//     if (parsed.ACCOUNTS) {
-//       if (!Array.isArray(parsed.ACCOUNTS)) {
-//         makeLogEntry("Accounts:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
-//       } else {
-//         listEntries("Accounts:", parsed.ACCOUNTS, ACCOUNT_FIELDS, async function(acct) {
-//           if (acct.address === undefined) {return undefined}
-//           let net
-//           if (acct.network === undefined) { net = "main" } else { net = acct.network }
-//           return await validateAddressDomainOnNet(acct.address, domain, net)
-//         })
-//       }
-//     }
-//     if (parsed.VALIDATORS) {
-//       if (!Array.isArray(parsed.VALIDATORS)) {
-//         makeLogEntry("Validators:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
-//       } else {
-//         listEntries("Validators:", parsed.VALIDATORS, VALIDATOR_FIELDS)
-//       }
-//     }
-//     if (parsed.PRINCIPALS) {
-//       if (!Array.isArray(parsed.PRINCIPALS)) {
-//         makeLogEntry("Principals:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
-//       } else {
-//         listEntries("Principals:", parsed.PRINCIPALS, PRINCIPAL_FIELDS)
-//       }
-//     }
-//     if (parsed.SERVERS) {
-//       if (!Array.isArray(parsed.SERVERS)) {
-//         makeLogEntry("Servers:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
-//       } else {
-//         listEntries("Servers:", parsed.SERVERS, SERVER_FIELDS)
-//       }
-//     }
-//     if (parsed.CURRENCIES) {
-//       if (!Array.isArray(parsed.CURRENCIES)) {
-//         makeLogEntry("Currencies:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
-//       } else {
-//         listEntries("Currencies:", parsed.CURRENCIES, CURRENCY_FIELDS)
-//       }
-//     }
-//   }
+    if (parsed.ACCOUNTS) {
+      if (!Array.isArray(parsed.ACCOUNTS)) {
+        makeLogEntry("Accounts:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
+      } else {
+        parsed.ACCOUNTS.reduce()
+        listEntries("Accounts:", parsed.ACCOUNTS, ACCOUNT_FIELDS, async function(acct) {
+          if (acct.address === undefined) {
+            return undefined
+          }
+          // let net
+          // if (acct.network === undefined) { net = "main" } else { net = acct.network }
+          const net = acct.network ?? "main"
+          // TODO: Migrate this next
+          // return await validateAddressDomainOnNet(acct.address, domain, net) 
+        })
+      }
+    }
+    // TODO: Break this into applications of a single function
+    // Make a log entry - If it's missing the field, error it instantly. Otherwise, list out the details of the field
+    if (parsed.VALIDATORS) {
+      if (!Array.isArray(parsed.VALIDATORS)) {
+        makeLogEntry("Validators:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
+      } else {
+        listEntries("Validators:", parsed.VALIDATORS, VALIDATOR_FIELDS)
+      }
+    }
+    if (parsed.PRINCIPALS) {
+      if (!Array.isArray(parsed.PRINCIPALS)) {
+        makeLogEntry("Principals:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
+      } else {
+        listEntries("Principals:", parsed.PRINCIPALS, PRINCIPAL_FIELDS)
+      }
+    }
+    if (parsed.SERVERS) {
+      if (!Array.isArray(parsed.SERVERS)) {
+        makeLogEntry("Servers:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
+      } else {
+        listEntries("Servers:", parsed.SERVERS, SERVER_FIELDS)
+      }
+    }
+    if (parsed.CURRENCIES) {
+      if (!Array.isArray(parsed.CURRENCIES)) {
+        makeLogEntry("Currencies:").resolve("Wrong type - should be table-array").addClass(CLASS_BAD)
+      } else {
+        listEntries("Currencies:", parsed.CURRENCIES, CURRENCY_FIELDS)
+      }
+    }
+  }
 
 async function fetchFile(
     setLogEntries: React.Dispatch<React.SetStateAction<JSX.Element[]>>,
@@ -393,11 +421,10 @@ async function fetchFile(
         
         if (accountToVerify){
             parseXRPLTomlWallet(setLogEntries, data, accountToVerify)
-        } 
-        // TODO: Next functions to migrate :) - May or may not also need the wallet address we're trying to verify
-        //   else{
-        //     parseXRPLToml(data, domain)
-        //   }
+        } else {
+          // TODO: Next functions to migrate :) - May or may not also need the wallet address we're trying to verify
+          parseXRPLToml(setLogEntries, data, domain)
+        }
     } catch (e) {
         const error = e as AxiosError
         console.log(error.status)
