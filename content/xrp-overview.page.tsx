40fea10f1b7854bd0e845cd570d88f0879cf42da
mDuo13 <mduo13@gmail.com>
2023-12-13 20:00:17 -0800

    Fix Redocly build

--- content/xrp-overview.page.tsx
+++ content/xrp-overview.page.tsx
@@ -109,7 +109,7 @@ export default function XrpOverview() {
         <div className="position-relative">
           <img
             alt="default-alt-text"
-            src={require("./static/img/backgrounds/xrp-overview-blue.svg")}
+            src={require("./img/backgrounds/xrp-overview-blue.svg")}
             className="landing-bg"
             id="xrp-overview-blue"
           />
@@ -253,7 +253,7 @@ export default function XrpOverview() {
                 <div className="mt-10 p-10 br-8 cta-card position-relative">
                   <img
                     alt="default-alt-text"
-                    src={require("./static/img/backgrounds/cta-xrp-overview-magenta.svg")}
+                    src={require("./img/backgrounds/cta-xrp-overview-magenta.svg")}
                     className="cta cta-bottom-right"
                   />
                   <div className="z-index-1 position-relative">
@@ -326,7 +326,7 @@ export default function XrpOverview() {
                 <div className="mt-10 p-10 br-8 cta-card position-relative">
                   <img
                     alt="default-alt-text"
-                    src={require("./static/img/backgrounds/cta-xrp-overview-green-2.svg")}
+                    src={require("./img/backgrounds/cta-xrp-overview-green-2.svg")}
                     className="landing-bg cta cta-bottom-right"
                   />
                   <div className="z-index-1 position-relative">
@@ -340,7 +340,7 @@ export default function XrpOverview() {
                         <img
                           id="xrp-mark-overview"
                           className="mw-100 invertible-img mr-2"
-                          src={require("../img/logos/xrp-mark.svg")}
+                          src={require("./img/logos/xrp-mark.svg")}
                           alt="XRP Logo Mark"
                         />
                         <span
