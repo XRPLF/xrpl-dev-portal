71c50b0514d62bcf2a81783a6d6bdea5030a865f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-12-17 15:44:39 +0900

    add fully AMM sample codes

---
+++ content/_code-samples/create-amm/ts/util/consts.ts
@@ -0,0 +1,3 @@
+// Connect to the network -----------------------------------------------------
+export const WS_URL = 'wss://s.devnet.rippletest.net:51233'
+export const EXPLORER = 'https://devnet.xrpl.org'
