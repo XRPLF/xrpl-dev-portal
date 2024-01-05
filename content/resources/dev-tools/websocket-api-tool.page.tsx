08a2b1f342567a531b43fb89553a132f81ec36e8
Omar Khan <khancodegt@gmail.com>
2024-01-05 00:42:31 -0500

    remove <span>

--- content/resources/dev-tools/websocket-api-tool.page.tsx
+++ content/resources/dev-tools/websocket-api-tool.page.tsx
@@ -197,13 +197,12 @@ export function WebsocketApiTool() {
                 </a>
               </h3>
               {currentMethod.description && (
-                <p className="blurb">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: currentMethod.description,
-                    }}
-                  />
-                </p>
+                <p
+                  className="blurb"
+                  dangerouslySetInnerHTML={{
+                    __html: currentMethod.description,
+                  }}
+                />
               )}
               {currentMethod.link && (
                 <a
