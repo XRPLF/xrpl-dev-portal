1346961475dc1bd5fc95894a444785b227cc03d1
JST5000 <aim4math@gmail.com>
2024-02-02 16:06:11 -0800

    Update checks snippets

--- _code-samples/checks/js/cancel-check.js
+++ _code-samples/checks/js/cancel-check.js
@@ -12,7 +12,7 @@ const main = async ()=> {
 
     // Check if the check ID is provided --------------------------------------
     if (checkId.length === 0) {
-      console.log("Please edit this snippet to provide a check ID. You can get a check ID by running create_check.js.");
+      console.log("Please edit this snippet to provide a check ID. You can get a check ID by running create-check.js.");
       return;
     }
     
