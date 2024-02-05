31d5483d7c99861f8093d54912bb68537a5ebf81
Amarantha Kulkarni <akulkarni@ripple.com>
2024-02-05 12:24:59 -0800

    [gh-pages] Rm stale previews 2024-02-05

--- pr-preview/rr-min-requirements/ja/assets/js/fixsidebarscroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-   var footerpos = parseInt($("footer").offset().top);
-   var scrollpos = $(window).scrollTop() + $(window).height();
-   if(scrollpos - footerpos > 0) {
-       $("aside.sidebar .dev_nav_wrapper").css("bottom", (scrollpos-footerpos)+"px");
-   } else {
-       $("aside.sidebar .dev_nav_wrapper").css("bottom", 0);
-   }
-});
