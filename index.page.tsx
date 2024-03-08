e28911f352538887e18701422315a743ab384b8d
akcodez <ariakesh@yahoo.com>
2024-03-08 10:49:51 -0800

    bring back | XRPL.org for homepage

--- index.page.tsx
+++ index.page.tsx
@@ -2,7 +2,7 @@ import { useTranslate } from '@portal/hooks';
 
 export const frontmatter = {
   seo: {
-    title: 'XRP Ledger Home',
+    title: 'XRP Ledger Home | XRPL.org',
     description: "XRPL.org is a community-driven site for the XRP Ledger (XRPL), an open-source, public blockchain. Gain access to technical documentation, reference materials, and blockchain ledger tools.",
   }
 };
