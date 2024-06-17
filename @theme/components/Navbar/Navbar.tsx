ff6ea9611ef92128b620180bb3184f9eb388518d
akcodez <ariakesh@yahoo.com>
2024-06-17 15:29:33 -0700

    fix alert banner show / hide logic, hide apex banner

--- @theme/components/Navbar/Navbar.tsx
+++ @theme/components/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ import { Search } from "@theme/components/Search/Search";
 // @ts-ignore
 
 const alertBanner = {
-  show: true,
+  show: false,
   message: "XRP Ledger Apex is back in Amsterdam",
   button: "Register Now",
   link: "https://www.xrpledgerapex.com/?utm_source=email&utm_medium=email_marketing&utm_campaign=EVENTS_XRPL_Apex_2024_Q2&utm_term=events_page_cta_button",
@@ -92,7 +92,7 @@ export function Navbar(props) {
         button={alertBanner.button}
         link={alertBanner.link}
       />
-      <NavWrapper belowAlertBanner={true}>
+      <NavWrapper belowAlertBanner={alertBanner.show}>
         <LogoBlock to={href} img={logo} alt={altText} />
         <NavControls>
           <MobileMenuIcon />
@@ -121,21 +121,24 @@ const StyledColorModeSwitcher = styled(ColorModeSwitcher)`
 `;
 
 export function AlertBanner(props) {
-  const { message, button, link } = props;
-  return (
-    <div className="top-banner fixed-top">
-      <div className="inner-apex">
-        <span>
-          <p className="mb-0 apex-banner-text">{message}</p>
-        </span>
-        <span>
-          <Link to={link} target="_blank" className="apex-btn">
-            {button}
-          </Link>
-        </span>
+  const { message, button, link, show } = props;
+  if (show) {
+    return (
+      <div className="top-banner fixed-top">
+        <div className="inner-apex">
+          <span>
+            <p className="mb-0 apex-banner-text">{message}</p>
+          </span>
+          <span>
+            <Link to={link} target="_blank" className="apex-btn">
+              {button}
+            </Link>
+          </span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return null;
 }
 
 export function TopNavCollapsible(props) {
@@ -174,7 +177,9 @@ export function NavDropdown(props) {
 
       return (
         <div key={index} className={clnm}>
-          <h5 className="dropdown-item">{translate(item.labelTranslationKey, item.label)}</h5>
+          <h5 className="dropdown-item">
+            {translate(item.labelTranslationKey, item.label)}
+          </h5>
           {groupLinks}
         </div>
       );
@@ -187,7 +192,7 @@ export function NavDropdown(props) {
         hero_href = pathPrefix + hero_href;
       }
       const splitlabel = item.label.split(" || ");
-      let splittranslationkey = ["",""]
+      let splittranslationkey = ["", ""];
       if (item.labelTranslationKey) {
         splittranslationkey = item.labelTranslationKey.split(" || ");
       }
