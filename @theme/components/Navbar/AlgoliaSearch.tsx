c29e62b17f3ddefd2c1589cd2f58919c7b06b79d
mDuo13 <mduo13@gmail.com>
2024-12-06 13:16:44 -0800

    Support localized search

--- @theme/components/Navbar/AlgoliaSearch.tsx
+++ @theme/components/Navbar/AlgoliaSearch.tsx
@@ -1,13 +1,19 @@
 import { DocSearch } from '@docsearch/react';
+import { useThemeHooks } from '@redocly/theme/core/hooks';
 
 export function AlgoliaSearch() {
+    const { useL10n } = useThemeHooks()
+    let { lang } = useL10n()
+    if (lang == "en-US") {
+        lang = "en"
+    }
     return (
             <DocSearch
                 appId="R39QY3MZC7"
                 indexName="xrpl"
                 apiKey="3431349deec23b0bc3dcd3424beb9a6e"
                 searchParameters={{
-                    facetFilters: ['lang:en'],
+                    facetFilters: ['lang:'+lang],
                 }}
             />
     )
