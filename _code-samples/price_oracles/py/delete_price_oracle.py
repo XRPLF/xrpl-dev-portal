3a18f8562acfb24dab59576f8662254883f0f355
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2025-01-09 09:43:23 +0100

    Update delete_price_oracle.py

--- _code-samples/price_oracles/py/delete_price_oracle.py
+++ _code-samples/price_oracles/py/delete_price_oracle.py
@@ -12,8 +12,11 @@ print("connecting to the test network")
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
 print("connected!!!")
 
+# input the seed that was generated from running the did_set.py
+seed = input("now, enter the seed of the account that owns a Price Oracle object to delete: ")
+
 # create demo wallet or use an existing one as created in the oracle set transaction
-oracle_creator = Wallet.from_seed(seed="sEdTXKysakqYD1eRaBmYAGvmwFRbNWa")
+oracle_creator = Wallet.from_seed(seed=seed)
 
 # define the oracle document id
 # this should be stored offline as the blockchain doesn't retrieve it in requests
@@ -39,6 +42,6 @@ oracle_set_txn_response = submit_and_wait(
 print(oracle_set_txn_response.result["meta"]["TransactionResult"])
 print(oracle_set_txn_response.result["hash"])
 
-# check if the transaction was successfull
+# check if the transaction was successful
 if oracle_set_txn_response.result["meta"]["TransactionResult"] == "tesSUCCESS":
-    print("oracle deleted succesfully")
+    print("oracle deleted successfully")
