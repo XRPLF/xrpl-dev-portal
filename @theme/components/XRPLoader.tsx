74eb5c87c7b7975c727d231f407bb89fe7250164
mDuo13 <mduo13@gmail.com>
2025-01-15 03:49:44 -0800

    Unify & fix XRPLoader component in dev tools

--- @theme/components/XRPLoader.tsx
+++ @theme/components/XRPLoader.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 
 export interface XRPLoaderProps {
     message?: string
+    show: boolean
 }
 
 export default function XRPLoader(props: XRPLoaderProps) {
+  const classnames = props.show ? "loader" : "loader collapse"
   return (
-  <div className="loader">
-    <img alt="(loading)" className="throbber" src="/img/xrp-loader-96.png" />
-    {props.message}
-  </div>);
+    <div className={classnames}>
+      <img alt="(loading)" className="throbber" src="/img/xrp-loader-96.png" />
+      {props.message}
+    </div>
+  )
 }
