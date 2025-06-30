2ce1d44608a1a6450d0754fa92cd43890aa2295c
Oliver Eggert <oeggert@ripple.com>
2025-06-30 11:27:35 -0700

    testing preview build imports

--- resources/dev-tools/tx-sender.page.tsx
+++ resources/dev-tools/tx-sender.page.tsx
@@ -4,8 +4,12 @@ import { useThemeHooks } from '@redocly/theme/core/hooks';
 import { Link } from '@redocly/theme/components/Link/Link';
 
 import AlertTemplate from './components/AlertTemplate';
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import { useAlert } from 'react-alert'
+import {
+    Provider as AlertProvider,
+    transitions,
+    positions,
+    useAlert
+} from 'react-alert/dist/esm/react-alert.js';
 
 import { isoTimeToRippleTime, type Client, type Wallet } from 'xrpl'
 
