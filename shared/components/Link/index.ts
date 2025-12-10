a86ebe23d81b0ca4683129230e603994cb897c6e
akcodez <ariakesh@yahoo.com>
2025-12-10 13:53:21 -0800

    rename link

--- shared/components/Link/index.ts
+++ shared/components/Link/index.ts
@@ -1,5 +1,5 @@
-export { Link } from './Link';
-export type { LinkProps, LinkVariant, LinkSize, LinkIconType } from './Link';
+export { BdsLink } from './Link';
+export type { BdsLinkProps, LinkVariant, LinkSize, LinkIconType } from './Link';
 
 export { LinkArrow } from './LinkArrow';
 export type { LinkArrowProps, LinkArrowVariant, LinkArrowSize } from './LinkArrow';
