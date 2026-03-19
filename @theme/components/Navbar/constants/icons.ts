efe0aa943b5d019b27a9c9f82f53cc77e929b8ef
Calvin <8109978+Calvinjwala@users.noreply.github.com>
2026-03-18 17:12:03 -0700

    Navbar icon updates: new 18×18 icons, dedicated Community icons, and code cleanup (#3544)
    * updating navigation with the correct icons
    
    * clean up code, add alt tags
    
    * consolidate, code clean up
    
    * minor tweak to icons.ts

--- @theme/components/Navbar/constants/icons.ts
+++ @theme/components/Navbar/constants/icons.ts
@@ -11,43 +11,11 @@ export { default as chevronDown } from "../../../../static/img/navbar/chevron-do
 export { default as hamburgerIcon } from "../../../../static/img/navbar/hamburger-icon.svg";
 export { default as arrowUpRight } from "../../../../static/img/icons/arrow-up-right-custom.svg";
 
-// Wallet icons for submenu
-export { default as greenWallet } from "../../../../static/img/navbar/green-wallet.svg";
-export { default as lilacWallet } from "../../../../static/img/navbar/lilac-wallet.svg";
-export { default as yellowWallet } from "../../../../static/img/navbar/yellow-wallet.svg";
-export { default as pinkWallet } from "../../../../static/img/navbar/pink-wallet.svg";
-export { default as blueWallet } from "../../../../static/img/navbar/blue-wallet.svg";
-
-// Develop submenu icons
-export { default as devHomeIcon } from "../../../../static/img/navbar/dev_home.svg";
-export { default as learnIcon } from "../../../../static/img/navbar/learn.svg";
-export { default as codeSamplesIcon } from "../../../../static/img/navbar/code_samples.svg";
-export { default as docsIcon } from "../../../../static/img/navbar/docs.svg";
-export { default as clientLibIcon } from "../../../../static/img/navbar/client_lib.svg";
-
-// Use Cases submenu icons
-export { default as paymentsIcon } from "../../../../static/img/navbar/payments.svg";
-export { default as tokenizationIcon } from "../../../../static/img/navbar/tokenization.svg";
-export { default as creditIcon } from "../../../../static/img/navbar/credit.svg";
-export { default as tradingIcon } from "../../../../static/img/navbar/trading.svg";
-
-// Community submenu icons
-export { default as communityIcon } from "../../../../static/img/navbar/community.svg";
-
-// Network submenu icons
-export { default as insightsIcon } from "../../../../static/img/navbar/insights.svg";
-export { default as resourcesIcon } from "../../../../static/img/navbar/resources.svg";
-
 // Network submenu pattern images (used for both light and dark mode)
 export { default as resourcesIconPattern } from "../../../../static/img/navbar/resources-icon.svg";
 export { default as insightsIconPattern } from "../../../../static/img/navbar/insights-icon.svg";
 
-// Wallet icon mapping for dynamic icon lookup
-import greenWallet from "../../../../static/img/navbar/green-wallet.svg";
-import lilacWallet from "../../../../static/img/navbar/lilac-wallet.svg";
-import yellowWallet from "../../../../static/img/navbar/yellow-wallet.svg";
-import pinkWallet from "../../../../static/img/navbar/pink-wallet.svg";
-import blueWallet from "../../../../static/img/navbar/blue-wallet.svg";
+// Submenu icons - imported once, exported individually and used in navIcons mapping
 import devHomeIcon from "../../../../static/img/navbar/dev_home.svg";
 import learnIcon from "../../../../static/img/navbar/learn.svg";
 import codeSamplesIcon from "../../../../static/img/navbar/code_samples.svg";
@@ -58,15 +26,33 @@ import tokenizationIcon from "../../../../static/img/navbar/tokenization.svg";
 import creditIcon from "../../../../static/img/navbar/credit.svg";
 import tradingIcon from "../../../../static/img/navbar/trading.svg";
 import communityIcon from "../../../../static/img/navbar/community.svg";
+import fundingIcon from "../../../../static/img/navbar/funding.svg";
+import contributeIcon from "../../../../static/img/navbar/contribute.svg";
+import ecosystemIcon from "../../../../static/img/navbar/ecosystem.svg";
 import insightsIcon from "../../../../static/img/navbar/insights.svg";
 import resourcesIcon from "../../../../static/img/navbar/resources.svg";
 
-export const walletIcons: Record<string, string> = {
-  green: greenWallet,
-  lilac: lilacWallet,
-  yellow: yellowWallet,
-  pink: pinkWallet,
-  blue: blueWallet,
+// Re-export submenu icons for direct imports
+export default {
+  devHomeIcon,
+  learnIcon,
+  codeSamplesIcon,
+  docsIcon,
+  clientLibIcon,
+  paymentsIcon,
+  tokenizationIcon,
+  creditIcon,
+  tradingIcon,
+  communityIcon,
+  fundingIcon,
+  contributeIcon,
+  ecosystemIcon,
+  insightsIcon,
+  resourcesIcon,
+};
+
+// Dynamic icon lookup for navbar submenus
+export const navIcons: Record<string, string> = {
   dev_home: devHomeIcon,
   learn: learnIcon,
   code_samples: codeSamplesIcon,
@@ -77,7 +63,9 @@ export const walletIcons: Record<string, string> = {
   credit: creditIcon,
   trading: tradingIcon,
   community: communityIcon,
+  contribute: contributeIcon,
   insights: insightsIcon,
   resources: resourcesIcon,
+  ecosystem: ecosystemIcon,
+  funding: fundingIcon,
 };
-
